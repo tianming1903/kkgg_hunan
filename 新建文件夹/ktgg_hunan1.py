b9b313beda04ae0b17e16f7083043bebaf486eb7
litianming <1438006060@qq.com>
2019-07-25 17:55:25 +0800

    7.25

--- 新建文件夹/ktgg_hunan1.py
+++ 新建文件夹/ktgg_hunan1.py
@@ -13,10 +13,39 @@ import hashlib
 
 class Ktgg_hunan(object):
     def __init__(self):
+        # 链接mysql数据库
+        self.db = pymysql.connect(host="localhost",user="root",password="123456",db='litianming')
+        self.cursor = self.db.cursor()
+
+        # 定义正则表达式
+        self.re = {
+            1: '原告(.*?)被告(.*?)案由(.*?)审判员(.*?)代理.*?开庭时间(.*?)开庭地点(.*)',
+            2: '原告(.*?)被告(.*?)案由(.*?)审判员(.*?)书记.*?开庭时间(.*?)开庭地点(.*)',
+            3: '原告(.*?)被告(.*?)案由(.*?)审判员(.*?)开庭时间(.*?)开庭地点(.*)',
+            4: '原告(.*?)被告(.*?)案由(.*?)开庭时间(.*?)开庭地点(.*?)审判员(.*)',
+            5: '原告(.*?)被告(.*?)开庭时间(.*?)开庭地点(.*?)审判员(.*)',
+        }
+
+        self.re1 = [
+            '于(.*?)\\d{1,2}:\\d{1,2}在(.*?)开庭审理(.*?)诉(.*?)%s',
+            '院(.*?)\\d{1,2}:\\d{1,2}在(.*?)开庭审理(.*?)诉(.*?)%s',
+            '于(.*?)\\d{1,2}:\\d{1,2}(.*?)开庭审理(.*?)诉(.*?)%s',
+            '院(.*?)\\d{1,2}:\\d{1,2}(.*?)开庭审理(.*?)诉(.*?)%s'
+            '院(.*?):\\d{1,2}(.*?)开庭审理(.*?)诉(.*?)%s',
+            '于(.*?)\\d{1,2}:\\d{1,2}在(.*?)开庭审理(.*?)诉(.*)'
+            ]
+        
+        self.re2 = [
+            '于(.*?)\\d{1,2}:\\d{1,2}在(.*?)开庭审理(.*?)%s',
+            '院(.*?)\\d{1,2}:\\d{1,2}在(.*?)开庭审理(.*?)%s',
+        ]
+        
         # 定义案由
         self.anyou = ''
-        # 定义标识词
-        self.biaoshi = ['纠纷','争议','诉']
+
+        # 定义去除详情页非数据标识
+        self.biaoshi = ['纠纷','争议','诉','开民','开庭公告']
+
         # 定义请求头headers
         self.headers = {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
@@ -26,6 +55,7 @@ class Ktgg_hunan(object):
             "Host": "kfqfy.chinacourt.gov.cn",
             "Upgrade-Insecure-Requests": "1",
             }
+
         # 起始url 
         self.url = "http://kfqfy.chinacourt.gov.cn/article/index/id/M0guMjCwMDAwNCACAAA/page/1.shtml"
 
@@ -37,7 +67,8 @@ class Ktgg_hunan(object):
     # 构建一级链接请求并且请求获得响应
     def set_request(self):
         print('正在获取详情页链接....')
-        i = 1
+        # 更改起始页
+        i = 10
         link_list = []
         while True:
             new = str(i) + '.shtml'
@@ -51,7 +82,12 @@ class Ktgg_hunan(object):
                 continue
             text = etree.HTML(html)
             urls = text.xpath('//div[@class="font14 dian_a"]//li//a/@href')
-            link_list.extend(urls) 
+            names = text.xpath('//div[@class="font14 dian_a"]//li//a/text()')
+            for x,y in zip(names,urls):
+                for s in self.biaoshi:
+                    if s in x:
+                        link_list.append(y)
+                        break
             if urls == []:
                 break
             i += 1
@@ -70,7 +106,9 @@ class Ktgg_hunan(object):
                     html = re.text
                 except (Timeout,ConnectionError):
                     continue
-                break   
+                break
+        
+            # 获取所有的文本内容
             text = etree.HTML(html)
             content = text.xpath('//div[@class="text"]')
             if content == []:
@@ -81,25 +119,23 @@ class Ktgg_hunan(object):
             d['source'] = self.url
             d['url'] = url
             d['title'] = text.xpath('//div[@class="b_title"]/text()')[0]
-            court =  text.xpath('//div[@class="from"]/text()')
-            # 没有则默认法院
-            if court == []:
-                d['court'] = '湖南省长沙市开福区人民法院'
-            else:
-                d['court'] = court[0].split('：')[1]
+            d['court'] = '湖南省长沙市开福区人民法院'
             d['posttime'] = text.xpath('//div[@class="sth_a"]/span[1]/text()')[0].strip().split('：')[-1]
             d['province'] = '湖南省'
             self.parse(info,d)
 
     # 解析文本提取字段
     def parse(self,info,d):
+        # 由于详情页的不同分为两种情况处理
+         # 由于详情页的不同分为两种情况处理
         if '开庭公告' in d['title']:
             info_list = re.findall('(.*?)案。',info)
             for x in info_list:
-                md5 = hashlib.md5()
-                l = []
                 t = x.strip('一').strip()
                 d['body'] = t
+
+                # 获取案由
+                l = []
                 for anyou in self.anyou:
                     if anyou.decode('utf-8') in t:
                         l.append(anyou.decode('utf-8'))
@@ -107,38 +143,83 @@ class Ktgg_hunan(object):
                 try:
                     d['anyou'] = l[0]
                 except IndexError:
-                    with open('shibai.txt','a',encoding='utf-8') as f:
-                        f.write(t)
-                        f.write('\n')
                     continue
-                try:
-                    if '诉' in t:
-                        text = re.findall('于(.*?)在(.*?)开庭审理(.*?)诉(.*?)%s' % d['anyou'] ,t)[0]
+
+                # 提取原告，被告，法庭，开庭时间(分为有误原告)
+                for i in self.re1:
+                    try:
+                        text = re.findall(i % d['anyou'] ,t)[0]
                         d['plaintiff'] = text[2]
                         d['pname'] = text[3]
+                        break
+                    except IndexError:
+                        continue
+                else:
+                    for i in self.re2:
+                        try:
+                            text = re.findall(i % d['anyou'] ,t)[0]
+                            d['plaintiff'] = ''
+                            d['pname'] = text[2]
+                            break
+                        except IndexError:
+                            continue
                     else:
-                        text = re.findall('于(.*?)在(.*?)开庭审理(.*?)%s'% d['anyou'] ,t)[0]
-                        d['plaintiff'] = ''
-                        d['pname'] = text[2]
-                    d['sorttime'] = text[0]
-                    d['courtNum'] = text[1]
-                except IndexError:
-                    with open('shibai.txt','a',encoding='utf-8') as f:
-                        f.write(t)
-                        f.write('\n')
-                    continue
-                md5.update((t + d['url']).encode())
+                        with open('shibai.txt','a',encoding='utf-8') as f:
+                            f.write('----------' + t)
+                            f.write('\n')
+                        continue
+                d['sorttime'] = text[0].strip()
+                d['courtNum'] = text[1].strip()
+
+                # 对被告和原告的后面多余字进行丢弃
+                d['pname'] = d['pname'].replace('在','')
+                d['plaintiff'] = d['plaintiff'].replace('在','')
+
+                md5 = hashlib.md5()
+                md5.update((d['body'] + d['url']).encode())
                 d['md5'] = md5.hexdigest()
-                print(d)
                 self.insert_mysql(d)
-
         else:
-            for y in self.biaoshi:
-                if y in d['title']:
-                    break
-
+            if '第三人' in info:
+                return
+            d['body'] = info.strip()
+            for x,y in self.re.items():
+                try:
+                    l = re.findall(y,info,re.S)[0]
+                except IndexError:
+                    continue
+                if '被告' in l[1]:
+                    return
+                if x == 4:
+                    d['anyou'] = l[2].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                    d['sorttime'] = l[3].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                    d['courtNum'] = l[4].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                    d['judge'] = l[5].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                elif x == 5:
+                    d['anyou'] = ''
+                    d['sorttime'] = l[2].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                    d['courtNum'] = l[3].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                    d['judge'] = l[4].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                else:
+                    d['anyou'] = l[2].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                    d['judge'] = l[3].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                    d['sorttime'] = l[4].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                    d['courtNum'] = l[5].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                d['plaintiff'] = l[0].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                d['pname'] = l[1].replace(';',' ').replace(':',' ').replace('：',' ').strip()
+                break
+            else:
+                with open('shibai.txt','a',encoding='utf-8') as f:
+                    f.write('----------' + d['body'])
+                    f.write('\n')
+                return
+            md5 = hashlib.md5()
+            md5.update((d['body'] + d['url']).encode())
+            d['md5'] = md5.hexdigest()
+            self.insert_mysql(d)
+            
+    # 对数据的入库和清洗
     def insert_mysql(self,d):
-
         # 删除没有值的字段
         l = []
         for i in d.keys():
@@ -148,31 +229,27 @@ class Ktgg_hunan(object):
             del d[i]
 
         # 准备入库
-        db = pymysql.connect(host="localhost",user="root",password="123456",db='litianming')
-        cursor = db.cursor()
         table = 'ktgg_hunan1'
         keys = ','.join(d.keys())
         values = ','.join(['%s'] * len(d))
         sql = "INSERT INTO {table}({keys}) VALUES({values})".format(table = table,keys = keys,values = values)
         try:
-            cursor.execute(sql,tuple(d.values()))
-            db.commit()
+            self.cursor.execute(sql,tuple(d.values()))
+            self.db.commit()
         except:
-            db.rollback()
-        cursor.close()
-        db.close()
+            self.db.rollback()
+    
+    def close_mysql(self):
+        self.cursor.close()
+        self.db.close()
 
+    # 函数的控制
     def main(self):
         self.set_anyou()
         links = self.set_request()
         self.request_info(links)
+        self.close_mysql()
 
-# if __name__ == "__main__":
-#     kh = Ktgg_hunan()
-#     kh.main()
-
-body = '开福区人民法院将于2018年10月30日14:00在B216民事审判庭开庭审理湖南爱屋装饰有限责任公司诉曹宇夫装饰装修合同纠纷'
-url = 'http://kfqfy.chinacourt.gov.cn/article/detail/2018/10/id/3543002.shtml'
-md5 = hashlib.md5()
-md5.update((body+url).encode())
-print(md5.hexdigest())
+if __name__ == "__main__":
+    kh = Ktgg_hunan()
+    kh.main()
